23eede60d63713fae41cbd00dceb86c5be2d82fe
iqbalsalik <sisalik84@gmail.com>
2024-09-11 02:57:15 +0530

    home solution bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -542,66 +542,12 @@ window.addEventListener('load', function() {
           targetElement.scrollIntoView({ behavior: 'smooth' });
       }
       const anchorTag = targetElement.querySelector('.serviceCategoryToClickDescription');
-      console.log(anchorTag)
       if (anchorTag) {
         anchorTag.click();
       }
 
   }
 });
-
-
-const images = document.querySelectorAll('img[data-toggle="modal"][data-target="#modal1"]');
-
-// Get the modal image element
-const modalImg = document.querySelector('.modal-img');
-
-// Get the prev and next navigation elements
-const prevNav = document.querySelector('.prev');
-const nextNav = document.querySelector('.next');
-
-// Initialize the current image index
-let currentIndex = 0;
-
-// Function to update the modal image
-function updateModalImage(index) {
-  modalImg.src = images[index].src;
-  currentIndex = index;
-}
-
-// Add event listeners to the navigation elements
-prevNav.addEventListener('click', () => {
-  updateModalImage((currentIndex - 1 + images.length) % images.length);
-});
-
-nextNav.addEventListener('click', () => {
-  updateModalImage((currentIndex + 1) % images.length);
-});
-
-// Update the modal image when an image is clicked
-images.forEach((image, index) => {
-  image.addEventListener('click', () => {
-    updateModalImage(index);
-  });
-});
-
-// Add keyboard navigation
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'ArrowLeft') {
-    updateModalImage((currentIndex - 1 + images.length) % images.length);
-  } else if (event.key === 'ArrowRight') {
-    updateModalImage((currentIndex + 1) % images.length);
-  }
-});
-
-
-
-
-
-
-
-
-
 
 // HOME PAGE SOLUTION
 
@@ -658,6 +604,16 @@ thumbnails.forEach((thumbnail, index) => {
         showSlider();
     })
 })
+
+
+
+
+
+
+
+
+
+
 
 
 
